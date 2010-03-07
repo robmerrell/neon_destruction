40b82a7b854e9752ca2233f73f2fcc154e708e3e
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2010-03-06 21:54:07 -0700

    Translate sprite coordinates into openGL translations

--- src/projection_engine/sprite.cpp
+++ src/projection_engine/sprite.cpp
@@ -1,10 +1,11 @@
 #include "sprite.h"
 
 Sprite::Sprite(std::string filename) {
-
 }
 
 void Sprite::display() {
+  glTranslatef(0.0, 0.0, 0.0);
+  
   glBegin(GL_TRIANGLE_STRIP);
     glColor4f(1.0, 0.0, 0.0, 1.0);
     glVertex2i(0, 100);
@@ -24,6 +25,7 @@ float Sprite::getX() {
 
 void Sprite::setX(float new_x) {
   x = new_x;
+  glTranslatef(x, 0.0, 0.0);
 }
 
 float Sprite::getY() {
@@ -32,6 +34,7 @@ float Sprite::getY() {
 
 void Sprite::setY(float new_y) {
   y = new_y;
+  glTranslatef(0.0, y, 0.0);
 }
 
 float Sprite::getWidth() {
--- src/scenes/intro_scene.cpp
+++ src/scenes/intro_scene.cpp
@@ -6,6 +6,8 @@ void IntroScene::setup() {
   // add the logo image
   // add the logo mask
   Sprite *logo = new Sprite("");
+  logo->setX(100);
+  logo->setY(100);
   addObject(logo);
   
   // start the game loop
@@ -15,15 +17,5 @@ void IntroScene::setup() {
 
 void IntroScene::gameLoop() {
   
-  
-  //   glBegin(GL_TRIANGLE_STRIP);
-  //     glColor4f(1.0,1.0,1.0,1.0);
-  //  glTexCoord2f(0.1, 0.9); glVertex2i(0,200); //v0
-  //  glTexCoord2f(0.1, 0.1); glVertex2i(0, 0);    //v1
-  //  glTexCoord2f(0.9, 0.9); glVertex2i(200,200);     ///v2
-  //  glTexCoord2f(0.9, 0.1); glVertex2i(200,0);  //v3
-  //  glTexCoord2f(0.9, 0.9); glVertex2i(400,200);   //v4
-  //  glTexCoord2f(0.9, 0.1); glVertex2i(400, 0);    //v5
-  // glEnd();
 }
 
