035c653887f2af623462df7912c69598428577d8
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2010-03-22 21:33:15 -0600

    Refined the projection multiplier

--- src/game_const.h
+++ src/game_const.h
@@ -10,7 +10,7 @@ const int BALL_TAG = 2;
 const int CANNON_COLLISION = 1;
 const int BALL_COLLISION = 2;
 
-const int PROJECTION_MULTIPLIER = 20;
+const int PROJECTION_MULTIPLIER = 25;
 
 
 /************
