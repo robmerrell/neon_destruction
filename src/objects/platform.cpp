747dae5538f604fab254577e943dc94fc07ada66
Rob Merrell <rmerrell@go1on1.com>
2010-04-29 12:35:19 -0600

    Cleaned up the platform rendering code

--- src/objects/platform.cpp
+++ src/objects/platform.cpp
@@ -22,41 +22,11 @@ void Platform::definePhysics(cpSpace *space) {
 }
 
 void Platform::display() {
+  // draw the frakkin line
   glLineWidth(4.0);
   glDisable(GL_TEXTURE_2D);
 
   glEnable(GL_LINE_SMOOTH);
-  
-  // glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
-  
-  /*
-  float dx = end_x - start_x;
-  float dy = end_y - start_y;
-  
-  GLfloat vertices[] = {0,6,0, dx,6,0, 0,0,0, dx,0,0};
-  GLfloat tex[] = {0,1,0, 1,1,0, 0,0,0, 1,0,0};
-  
-  
-  TexManager::Instance()->bindTexture(6);
-  
-  glLoadIdentity();
-  glTranslatef(start_x, start_y, 0.0);
-  
-  // rotate if needed
-  if (dy != 0) {
-    float angle = getInvertedMouseAngle(cpv(start_x, start_y), cpv(end_x, end_y));
-    glRotatef(angle, 0.0, 0.0, 1.0);
-  }
-  
-  glEnableClientState(GL_VERTEX_ARRAY);
-  
-  glVertexPointer(3, GL_FLOAT, 0, vertices);
-  
-  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-  
-  glDisableClientState(GL_VERTEX_ARRAY);
-  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-  */
   glLoadIdentity();
   glTranslatef(0, 0, 0);
   
@@ -68,46 +38,12 @@ void Platform::display() {
   glDisableClientState(GL_VERTEX_ARRAY);
   
   glEnable(GL_TEXTURE_2D);
-  // glEnable(GL_BLEND);
-  // glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_COLOR);
-  
-  
-  
-  /*
+
+
   // draw the glow
   float dx = end_x - start_x;
-  float dy = end_y - start_y;
-  
-  GLfloat glow_vertices[] = {0,32,0, dx,32,0, 0,0,0, dx,0,0};
-  GLfloat tex[] = {0,1,0, 1,1,0, 0,0,0, 1,0,0};
-  
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  TexManager::Instance()->bindTexture(7);
-  
-  glLoadIdentity();
-  glTranslatef(start_x, start_y, 0.0);
-  
-  // rotate if needed
-  // if (dy != 0) {
-  //   float angle = getInvertedMouseAngle(cpv(start_x, start_y), cpv(end_x, end_y));
-  //   glRotatef(angle, 0.0, 0.0, 1.0);
-  // }
-  
-  glEnableClientState(GL_VERTEX_ARRAY);
-  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  
-  glVertexPointer(3, GL_FLOAT, 0, glow_vertices);
-  
-  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-  
-  glDisableClientState(GL_VERTEX_ARRAY);
-  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-  */
-  
-  float dx = end_x - start_x;
-  float dy = end_y - start_y;
-  float distance = sqrt((dx*dx) - (dy * dy));
+  float dy = start_y - end_y;
+  float distance = sqrt((dx*dx) + (dy * dy));
   
   GLfloat shadow_vertices[] = {0,32,0, distance,32,0, 0,0,0, distance,0,0};
   GLfloat tex[] = {0,1,0, 1,1,0, 0,0,0, 1,0,0};
@@ -115,7 +51,7 @@ void Platform::display() {
   TexManager::Instance()->bindTexture(7);
   
   glLoadIdentity();
-  glTranslatef(start_x, start_y - 16, 0.0);
+  glTranslatef(start_x, start_y, 0.0);
   
   // rotate if needed
   if (dy != 0) {
@@ -135,40 +71,4 @@ void Platform::display() {
   glDisableClientState(GL_TEXTURE_COORD_ARRAY);
   
   TexManager::Instance()->unbindTexture();
-  
-  
-  
-  /*
-  // GLfloat ball_vertices[] = {0,6,0, ,6,0, 0,0,0, 64,0,0};
-  float dx = end_x - start_x;
-  float dy = end_y - start_y;
-  
-  GLfloat vertices[] = {0,6,0, dx,6,0, 0,0,0, dx,0,0};
-  GLfloat tex[] = {0,1,0, 1,1,0, 0,0,0, 1,0,0};
-  
-  
-  TexManager::Instance()->bindTexture(6);
-  
-  glLoadIdentity();
-  glTranslatef(start_x, start_y, 0.0);
-  
-  // rotate if needed
-  if (dy != 0) {
-    float angle = getInvertedMouseAngle(cpv(start_x, start_y), cpv(end_x, end_y));
-    glRotatef(angle, 0.0, 0.0, 1.0);
-  }
-  
-  glEnableClientState(GL_VERTEX_ARRAY);
-  glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-  
-  glVertexPointer(3, GL_FLOAT, 0, vertices);
-  glTexCoordPointer(3, GL_FLOAT, 0, tex);
-  
-  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-  
-  glDisableClientState(GL_VERTEX_ARRAY);
-  glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-  
-  TexManager::Instance()->unbindTexture();
-  */
 }
