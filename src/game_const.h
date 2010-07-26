c2a47960c1054ce768c03b101073e90448f6a890
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2010-07-25 21:14:37 -0600

    Added menu instructions

--- src/game_const.h
+++ src/game_const.h
@@ -50,7 +50,7 @@ const int SCREEN_BPP = 32;
 /*************
 ** Graphics **
 **************/
-const int TEXTURE_COUNT = 24;
+const int TEXTURE_COUNT = 25;
 const int STARS_PER_FIELD = 170;
 const float STAR_FIELD1_SPEED = 100.0f;
 const float STAR_FIELD2_SPEED = 50.0f;
