363346ad60b9e987beb6dc8a6931e29c71f5edc6
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2010-08-10 22:55:28 -0600

    Cleaned up the title scene

--- src/scenes/title_scene.cpp
+++ src/scenes/title_scene.cpp
@@ -3,32 +3,12 @@
 TitleScene::TitleScene() {
   quit = false;
   loaded = false;
+  playing_music = false;
 }
 
 TitleScene::~TitleScene() {}
 
 bool TitleScene::setup() {
-  int x, y = 0;
-  srand((unsigned)time(0)); 
-  for (int i = 0; i < STARS_PER_FIELD; i++) {
-    x = 1 + rand() % (SCREEN_WIDTH);
-    y = 1 + rand() % (SCREEN_HEIGHT);
-
-    starfield1[i] = cpv(x, y);
-  
-    x = 1 + rand() % (SCREEN_WIDTH);
-    y = 1 + rand() % (SCREEN_HEIGHT);
-
-    starfield2[i] = cpv(x, y);
-  }
-  
-  defineStarColors();
-  
-  for (int i = 0; i < STARS_PER_FIELD * 2; i++) {
-    starfield_sizes[i] = 1 + rand() % (2);
-  }
-  
-  
   Image *image;
 
   image = new Image(0.0f, 0.0f, 512.0f, 512.0f, 0);
@@ -79,9 +59,7 @@ void TitleScene::gameLoop() {
     glDisable(GL_BLEND);
     
     // draw the background
-    // moveBackground(animation_ticks);
     updateAnimation(animation_ticks);
-    // drawBackground();
     
     // blending
     glEnable(GL_BLEND);
@@ -117,9 +95,7 @@ void TitleScene::gameLoop() {
       
       SoundManager::Instance()->loadMusic("assets/music.mp3");
       SoundManager::Instance()->loadEffects();
-      SoundManager::Instance()->soundOn(true);
-      SoundManager::Instance()->playMusic();
-
+            
       LevelData::Instance()->parseLevelList("core.xml");
       LevelData::Instance()->parseUserData();
       
@@ -131,98 +107,14 @@ void TitleScene::gameLoop() {
         (*sprite)->setAnimationState(ANIMATE_FADE_OUT);
       }
       
+      if (!playing_music && animation.get_ticks() >= 4000) {
+        SoundManager::Instance()->soundOn(true);
+        playing_music = true;
+      }
+      
       if (animation.get_ticks() >= 5000) {
         in_loop = false;
       }
     }
-  }
-}
-
-void TitleScene::drawBackground() {
-  TexManager::Instance()->unbindTexture();
-
-  int v_count = 0;
-
-  GLfloat star_verts[STARS_PER_FIELD * 4];
-  for (int i = 0; i < STARS_PER_FIELD; i++) {
-    star_verts[v_count] = starfield1[i].x;
-    v_count++;
-
-    star_verts[v_count] = starfield1[i].y;
-    v_count++;
-
-    star_verts[v_count] = starfield2[i].x;
-    v_count++;
-
-    star_verts[v_count] = starfield2[i].y;
-    v_count++;
-  }
-
-  glLoadIdentity();
-  glTranslatef(0, 0, 0);
-
-  glEnableClientState(GL_VERTEX_ARRAY);
-  glEnableClientState(GL_COLOR_ARRAY);
-  glEnableClientState(GL_POINT_SIZE_ARRAY_OES);
-  
-  glVertexPointer(2, GL_FLOAT, 0, star_verts);
-  glColorPointer(4, GL_FLOAT, 0, starfield_colors);
-  glPointSizePointerOES(GL_FLOAT, 0, starfield_sizes);
-  glDrawArrays(GL_POINTS, 0, STARS_PER_FIELD * 2);
-  
-  glDisableClientState(GL_POINT_SIZE_ARRAY_OES);
-  glDisableClientState(GL_COLOR_ARRAY);
-  glDisableClientState(GL_VERTEX_ARRAY);
-}
-
-void TitleScene::moveBackground(int ticks) {
-  for (int i = 0; i < STARS_PER_FIELD; i++) {
-    starfield1[i].x -= (STAR_FIELD1_SPEED * (ticks * 0.001));
-    starfield2[i].x -= (STAR_FIELD2_SPEED * (ticks * 0.001));
-
-    if (starfield1[i].x < 0)
-      starfield1[i].x = SCREEN_WIDTH;
-    if (starfield2[i].x < 0)
-      starfield2[i].x = SCREEN_WIDTH;
-  }
-}
-
-void TitleScene::defineStarColors() {
-  int random_num, color_index = 0;
-  float r,g,b = 1.0f;
-  for (int i = 0; i < STARS_PER_FIELD*2; i++) {
-    random_num = 1 + rand() % (10);
-
-    // yellow
-    if (random_num == 1) {
-      r = 1.0f;
-      g = 0.99f;
-      b = 0.94f;
-    }
-    // blue
-    else if (random_num == 2) { 
-      r = 0.9f;
-      g = 0.99f;
-      b = 1.0f;
-    }
-    // red
-    else if (random_num == 3) {
-      r = 1.0f;
-      g = 0.9f;
-      b = 0.9f;
-    }
-    // white
-    else {
-      r = 1.0f;
-      g = 1.0f;
-      b = 1.0f;
-    }
-    
-    starfield_colors[color_index] = r;
-    starfield_colors[color_index + 1] = g;
-    starfield_colors[color_index + 2] = b;
-    starfield_colors[color_index + 3] = 1.0f;
-    
-    color_index += 4;
   }
 }
--- src/scenes/title_scene.h
+++ src/scenes/title_scene.h
@@ -12,17 +12,12 @@ class TitleScene : public Scene {
   private:
     bool quit;
     bool loaded;
+    bool playing_music;
     
     int frame;
     Timer fps;
     Timer animation;
     float animation_ticks;
-    
-    cpVect starfield1[STARS_PER_FIELD];
-    cpVect starfield2[STARS_PER_FIELD];
-    
-    float starfield_colors[(STARS_PER_FIELD * 2) * 4];
-    float starfield_sizes[STARS_PER_FIELD * 2];
     
   public:
     TitleScene();
@@ -30,21 +25,6 @@ class TitleScene : public Scene {
     
     bool setup();
     void gameLoop();
-    
-    /**
-     * Defines the colors used for the star background
-     */
-    void defineStarColors();
-    
-    /**
-     * Add the level background to a scene
-     */
-    void drawBackground();
-    
-    /**
-     * Moves the star fields
-     */
-    void moveBackground(int);
 };
 
 #endif
