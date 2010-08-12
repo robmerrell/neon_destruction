db3b2bc08c3b83dbafd9effe86c9dad839862bf0
Rob Merrell <rob@Rob-Merrells-MacBook.local>
2010-08-11 22:26:05 -0600

    Turned sound back on

--- src/scenes/title_scene.cpp
+++ src/scenes/title_scene.cpp
@@ -108,7 +108,7 @@ void TitleScene::gameLoop() {
       }
       
       if (!playing_music && animation.get_ticks() >= 4000) {
-        SoundManager::Instance()->soundOn(false);
+        SoundManager::Instance()->soundOn(true);
         playing_music = true;
       }
       
